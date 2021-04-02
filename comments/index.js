d677fa88a565a802113501534114a09e58383ef6
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-02 11:52:46 -0600

    sent created comment to event bus

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
 
 app.use(express.json());
@@ -9,18 +10,25 @@ app.use(cors());
 const commentsByPostID = {};
 
 app.get("/posts/:id/comments", (req, res) => {
-  res.send(commentsByPostID[req.params.id || []]);
+  res.send(commentsByPostID[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostID[req.params.id] || [];
 
   comments.push({ id, content });
-
   commentsByPostID[req.params.id] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comments);
 });
 
